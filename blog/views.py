f5a66e2e55ab8c10c787f9c908cadc2c85a49769
Eugene Reyek <y.rieiek.ip96@kpi.ua>
2022-04-24 17:13:13 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,22 @@
+import logging
+
 from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 
 
+logger = logging.getLogger(__name__)
+
+
 def index(request):
-  return render(request, 'blog/index.html', {'posts': Post.objects.all()})
+  posts = Post.objects.all()
+  logger.debug("Got %d posts", len(posts))
+  return render(request, 'blog/index.html', {'posts': posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
+    logger.info(
+        "Created comment on Post %d for user %s", post.pk, request.user
+    )
     return render(request, "blog/post-detail.html", {"post": post})
